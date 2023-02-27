6de7e23f2d5b84a5a3f48ac2e3c2b313edc3149a
Mushko2932 <103496282+Mushko2932@users.noreply.github.com>
2023-02-27 11:16:53 +0000

    Deploying to gh-pages from @ Mushko2932/goit-js-hw-09@3bab08bdcb26d02bc31fb7f5475b4c0bf65c5b9f 🚀

---
+++ 01-color-switcher.a1f3e862.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};let e=null;t.stop.disabled=!0;t.start.addEventListener("click",(()=>{t.start.disabled=!0,t.stop.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(()=>{t.stop.disabled=!0,t.start.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.a1f3e862.js.map
