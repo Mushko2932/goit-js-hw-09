3bab08bdcb26d02bc31fb7f5475b4c0bf65c5b9f
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-02-27 13:16:24 +0200

    завдання1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const DELAY = 1000;
+
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+
+let colorChangeInterval = null;
+refs.stop.disabled = true;
+
+const startChangeBackground = () => {
+    refs.start.disabled = true;
+    refs.stop.disabled = false;
+
+    colorChangeInterval = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, DELAY);
+};
+
+const stopChangeBackgroud = () => {
+    refs.stop.disabled = true;
+    refs.start.disabled = false;
+
+    clearInterval(colorChangeInterval);
+};
+
+refs.start.addEventListener('click', startChangeBackground);
+refs.stop.addEventListener('click', stopChangeBackgroud);
