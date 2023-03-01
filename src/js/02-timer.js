a47565edb1659d95336ecc56faea104046e68643
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-03-01 14:51:08 +0200

    завдання3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,25 @@
 import flatpickr from 'flatpickr';
-import convertMs from './converter';
+import { convertMs } from './converter';
+import {addLeadingZero} from './converter';
+import Notiflix from 'notiflix';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const DELAY = 1000;
+
+// отримуємо посилання
 const refs = {
-  input: document.querySelector('#datetime-picker'),
+  inputValue: document.querySelector('#datetime-picker'),
   btnStart: document.querySelector('button[data-start]'),
-  timer: document.querySelector('.timer'),
+  timerValue: document.querySelector('.timer'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
 
+refs.btnStart.disabled = true;
+
+// використовуємо бібліотеку flatpickr
 flatpickr('input#datetime-picker', {
   enableTime: true,
   time_24hr: true,
@@ -15,26 +27,49 @@ flatpickr('input#datetime-picker', {
   minuteIncrement: 1,
   onClose(selectedDates) {
       console.log(selectedDates[0]);
-      differenceDate(selectedDates[0]);
+      checkingSelectedDate(selectedDates[0]);
   },
 });
 
-function differenceDate(selectedDates) {
-    const currentDate = new Date();
+// обробляємо дату, обрану користувачем
+function checkingSelectedDate(selectedDates) {
+  const currentDate = new Date();
 
-    if (selectedDates <= currentDate) {
-        refs.btnStart.disabled = true;
-        return alert('Please choose a date in the future');
-    } else refs.btnStart.disabled = false;
+  if (selectedDates <= currentDate) {
+    Notiflix.Notify.warning('Please choose a date in the future');
+    refs.btnStart.disabled = true;
+  }
+  refs.btnStart.disabled = false;
 }
 
-refs.input.addEventListener('click', e => {
+// додаємо слухачів
+refs.inputValue.addEventListener('click', e => {
   e.preventDefault();
-  console.log(e.target.value);
+  //   console.log(e.target.value);
 });
 
 refs.btnStart.addEventListener('click', () => {
-    const intervalId = setInterval(() => {
-      
-    }, 1000);
+  // відлік часу
+    const timer = setInterval(() => {
+      let countdown = new Date(refs.inputValue.value) - new Date();
+      refs.btnStart.disabled = true;
+      if (countdown >= 0) {
+        const timeConvert = convertMs(countdown);
+
+        // додавати 0, якщо в числі менше двох символів
+        refs.days.textContent = addLeadingZero(timeConvert.days);
+        refs.hours.textContent = addLeadingZero(timeConvert.hours);
+        refs.minutes.textContent = addLeadingZero(timeConvert.minutes);
+        refs.seconds.textContent = addLeadingZero(timeConvert.seconds);
+      }
+
+      // зупиняємо таймер, коли дійшов до кінцевої дати
+      if (refs.minutes.textContent === 0 && refs.seconds.textContent === 0) {
+        Notiflix.Notify.success('sale time has arrived');
+        clearInterval(timer);
+      }
+    }, DELAY);
 });
+
+
+
